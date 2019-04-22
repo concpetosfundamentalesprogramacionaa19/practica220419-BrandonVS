f3503994af7af7c77b2132808d491abb8a5646f6
BrandonVS <vegasotobrandon@yahoo.com>
2019-04-22 16:20:48 -0500

    Demo

--- demo.py
+++ demo.py
@@ -5,11 +5,13 @@
 
 import sys
 
+# Se declaran las variables
 variable = sys.argv[0]
 dato1 = sys.argv[1]
 dato2 = sys.argv[2]
 dato3 = sys.argv[3]
 
+# Imprimimos las variables
 print ("variable argv[0]:	%s" % variable)
 print ("variable argv[1]:	%s" % dato1)
 print ("variable argv[2]:	%s" % dato2)
--- demo2.py
+++ demo2.py
@@ -1,10 +1,18 @@
+"""
+	Ejemplo de lenguaje python
+	autor @BrandonVS
+"""
+
 import sys
 
+# Se declaran las variables
 dato1 = sys.argv[1]
 dato2 = sys.argv[2]
 
-suma = int(dato1) + int(dato2) # aqui realizo la suma de variables
-
+# Realizamos la suma y multiplicacion
+suma = int(dato1) + int(dato2)
 multipl = int(dato1) * int(dato2)
 
-print ("La suma es: %s\nLa multiplicacion es: %s" % suma, multipl)
+# Imprimimos los resultados
+print ("La suma es: %s" % suma)
+print ("La multiplicacion es: %s" % multipl)
